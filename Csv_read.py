d3c74ae3a5bddc7638c1b8eaa4960932624abd55
alex-qwerty <35779934+alex-qwerty@users.noreply.github.com>
2019-03-17 20:17:22 +0300

    Add files via upload

---
+++ Csv_read.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+df = pd.read_csv('new.csv', encoding = 'cp1251', delimiter=';')
+
+brands = df['Бренд'].unique()
+
+for brand in brands:
+    file_name = brand + ".csv"
+    new_files = df[df['Бренд']==brand]
+    new_files.to_csv(file_name, index=False, encoding='cp1251')
+
+
